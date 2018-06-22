fef6cfe527a97bc510493ea0b7d5c7ad8b2022d9
Erin Mozdy <33384175+ecmozdy@users.noreply.github.com>
2018-06-21 19:26:32 -0700

    updated runner

--- runner.py
+++ runner.py
@@ -5,12 +5,3 @@ directory = 'runnerfiles'
 for filename in os.listdir(directory):
 	subprocess.call(['tcpdump','-r',directory + '/' + filename,'tcp','-w','runnertcp/'+filename])
 	subprocess.call(['tcpdump','-r',directory + '/' + filename,'udp','-w','runnerudp/'+filename])
-
-
-#sys.stdout = open('netflowoutput', 'w')
-second_directory = 'runnertcp'
-for filename in os.listdir(second_directory):
-	subprocess.call(['softflowd','-n','localhost:9995','-r',directory + '/' + filename])
-	subprocess.call(['nfcapd','-l','/var/cache/nfdump','-p','9995']) #-D maybe? Daemon mode
-	subprocess.call(['nfdump','-r','/var/cache/nfdump','-o','extended','-o','csv']) #'-r','/var/cache/nfdump']) --> reading from file
-									#-q removes headers and summary
